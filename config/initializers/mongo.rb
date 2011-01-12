7684870d1e9f3a177f044bc820aa256200d3dd4b
Yorick Sijsling <yoricksijsling@gmail.com>
2011-01-12 15:46:00 +0100

    Configuration for heroku

--- config/initializers/mongo.rb
+++ config/initializers/mongo.rb
@@ -1,3 +1,7 @@
-MongoMapper.database = "ergtraining-#{Rails.env}"
+if ENV['MONGOHQ_URL']
+  MongoMapper.config = { RAILS_ENV => { 'uri' => ENV['MONGOHQ_URL'] } }
+else
+  MongoMapper.database = "ergtraining-#{Rails.env}"
+end
 
 Team.ensure_index :title
